7546b2bea6d1d2ae151d5ad9f87aa0497169bcd6
Tran Van Kiet <trvankiet02@gmail.com>
2022-12-01 20:54:45 +0700

    Loi update user

--- admin/index.php
+++ admin/index.php
@@ -8,7 +8,7 @@ require_once('model/sinhvien_db.php');
 $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL) {
     $action = filter_input(INPUT_GET, 'action');
-    if ($action == NULL) {        
+    if ($action == NULL) {
         $action = 'register_gv';
         //if (isset($_SESSION['user'])) {
         //    $action = 'view_account';
@@ -25,15 +25,15 @@ switch ($action) {
         break;
 
     case 'register_sv':
-        if ($action == filter_input(INPUT_POST, 'action')){
-        //user
+        if ($action == filter_input(INPUT_POST, 'action')) {
+            //user
             $taikhoan = filter_input(INPUT_POST, 'taikhoan');
             $password = filter_input(INPUT_POST, 'password');
             $is_admin = 'false';
             $is_gv = 'false';
             $is_sv = 'true';
             $is_truongbomon = 'false';
-            
+
 
 
             //ttsinhvien
@@ -51,29 +51,27 @@ switch ($action) {
                 include 'admin/view_register_sv.php';
                 break;
             }
-          
-                        
-            if(add_user($taikhoan,$password,$is_admin,$is_gv,$is_sv,$is_truongbomon)) {
-                if(add_sinhvien($taikhoan,$hovaten,$ngaysinh,$gioitinh,$doituong,$ctdt,$lop,$chuyennganh,$tinchitichluy)){
+
+
+            if (add_user($taikhoan, $password, $is_admin, $is_gv, $is_sv, $is_truongbomon)) {
+                if (add_sinhvien($taikhoan, $hovaten, $ngaysinh, $gioitinh, $doituong, $ctdt, $lop, $chuyennganh, $tinchitichluy)) {
                     $message = 'Dang ky thanh cong';
-                    
                 } else {
                     $message = 'add sinh vien khong thanh cong';
                     include 'admin/view_register_sv.php';
                     break;
                 }
-            }else {
+            } else {
                 $message = 'add user khong thanh cong';
                 include 'admin/view_register_sv.php';
                 break;
             }
-        
         }
         include 'admin/view_register_sv.php';
         break;
     case 'register_gv':  //this is POST
         //not to get this action, just POST
-        if ($action == filter_input(INPUT_POST, 'action')){
+        if ($action == filter_input(INPUT_POST, 'action')) {
             //user
             $taikhoan = filter_input(INPUT_POST, 'taikhoan');
             $password = filter_input(INPUT_POST, 'password');
@@ -94,36 +92,64 @@ switch ($action) {
                 break;
             }
 
-            if ($password == ''){
+            if ($password == '') {
                 $message = 'Password ko duoc rong';
                 include 'admin/view_register_gv.php';
                 break;
             }
 
-            if ($hovaten == ''){
+            if ($hovaten == '') {
                 $message = 'Ho va ten ko duoc rong';
                 include 'admin/view_register_gv.php';
                 break;
             }
 
-            if(add_user($taikhoan,$password,$is_admin,$is_gv,$is_sv,$is_truongbomon)) {
-                if(add_giangvien($taikhoan,$hovaten,$cdkh,$chuyennganh,$chucvu)){
+            if (add_user($taikhoan, $password, $is_admin, $is_gv, $is_sv, $is_truongbomon)) {
+                if (add_giangvien($taikhoan, $hovaten, $cdkh, $chuyennganh, $chucvu)) {
                     $message = 'Dang ky thanh cong';
                 } else {
                     $message = 'add giang vien khong thanh cong';
                     include 'admin/view_register_gv.php';
                     break;
                 }
-            }else {
+            } else {
                 $message = 'add user khong thanh cong';
                 include 'admin/view_register_gv.php';
                 break;
             }
         }
+
+    case 'update_sv':
+        if ($action == filter_input(INPUT_POST, 'action')) {
+            //user
+            $taikhoan = filter_input(INPUT_POST, 'taikhoan');
+            $password = filter_input(INPUT_POST, 'password');
+
+
+
+
+            //ttsinhvien
+            $hovaten = filter_input(INPUT_POST, 'hovaten');
+            $ngaysinh = filter_input(INPUT_POST, 'ngaysinh');
+            $gioitinh = filter_input(INPUT_POST, 'gioitinh');
+            $doituong = filter_input(INPUT_POST, 'doituong');
+            $ctdt = filter_input(INPUT_POST, 'ctdt');
+            $lop = filter_input(INPUT_POST, 'lop');
+            $chuyennganh = filter_input(INPUT_POST, 'chuyennganh');
+            $tinchitichluy = filter_input(INPUT_POST, 'tinchitichluy');
+
+            
+                if (update_sinhvien($taikhoan, $hovaten, $ngaysinh, $gioitinh, $doituong, $ctdt, $lop, $chuyennganh, $tinchitichluy)) {
+                    $message = 'Update thanh cong';
+                    include 'admin/view_view_sv.php';
+                    break;
+                }
+                
+        }
+
         include 'admin/view_register_gv.php';
         break;
     default:
         display_error("Unknown account action: " . $action);
         break;
 }
-?>
