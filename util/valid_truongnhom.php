85932434435adaa7a1db8ebded94dd07f4b89b1a
benquang <114910044+benquang@users.noreply.github.com>
2022-12-12 16:24:42 +0700

    Update valid_truongnhom.php

--- util/valid_truongnhom.php
+++ util/valid_truongnhom.php
@@ -2,7 +2,6 @@
     // make sure the user is logged in as a valid administrator
     $detai = get_one_detai_by_sinhvien($_SESSION['user']);
     if ($detai['is_truongnhom'] != '1') {//ko phải là trưởng nhóm
-        $message = "Không phải là trưởng nhóm";
-        include 'sinhvien/profile.php';
+        header('Location: ' . $app_path . 'sinhvien?action=profile' );
     }
 ?>
